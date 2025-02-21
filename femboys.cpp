d01ce1cd9c4b034bccff206a3f761e8b3e52defc
AdarshSharma06 <164078693+AdarshSharma06@users.noreply.github.com>
2025-02-21 18:29:16 +0530

    Update femboys.cpp

--- femboys.cpp
+++ femboys.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main(){
-  cout<<"i like femboys"<<"\n"<<""\\i want to be a femboy\" - Adarsh Sharma";
+  cout<<"i like femboys"<<"\n"<<"\"i want to be a femboy\" - Adarsh Sharma";
   return 0;
 }
   
